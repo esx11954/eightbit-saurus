1215d7a6419dafd9ef04389f317f02877c6c462a
esx11954 <42484592+esx11954@users.noreply.github.com>
2025-05-19 10:47:09 +0900

    new django prj deploy proc

---
+++ docs/Python/Django/files/mysite.sql
@@ -0,0 +1,6 @@
+CREATE USER myuser WITH PASSWORD 'mypassword';
+ALTER ROLE myuser SET client_encoding TO 'utf8';
+ALTER ROLE myuser SET default_transaction_isolation TO 'read committed';
+ALTER ROLE myuser SET timezone TO 'UTC';
+CREATE DATABASE mydatabase with owner myuser;
+GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
--- docs/Python/Django/page1.md
+++ docs/Python/Django/page1.md
@@ -1,5 +1,6 @@
 ---
 sidebar_position: 1
+draft: true
 ---
 
 # デプロイ環境構築
